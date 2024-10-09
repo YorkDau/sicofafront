c6d6c1701e0c8ec2bc2012526602e4ac886c53a8
York Dau <dsflint02@gmail.com>
2024-10-09 11:06:44 -0500

    🚀 Initial commit: Stable version of the SICOFA frontend structure and core functionalities

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  name: 'production',
+  path: 'https://172.210.178.254/api',
+  
+  JWT_TOKEN: 'JWT_TOKEN',
+  USER_INFO: 'USER_INFO',
+  PERFILES_COMISARIAS: 'PERFILES_COMISARIAS',
+  REFRESH_TOKEN: 'REFRESH_TOKEN',
+};
