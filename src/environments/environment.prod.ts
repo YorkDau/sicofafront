44263a443d1b2ed1da77ae4463b785ae263e297e
York Dau <yorkdau10@gmail.com>
2025-02-06 04:47:31 -0800

    feat:variables de entorno para produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,7 @@
 export const environment = {
   production: true,
   name: 'production',
-  path: 'https://172.210.178.254/api',
-  
+  path: 'https://sicofaqa.cali.gov.co:4443/api',
   JWT_TOKEN: 'JWT_TOKEN',
   USER_INFO: 'USER_INFO',
   PERFILES_COMISARIAS: 'PERFILES_COMISARIAS',
--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
       name: 'development',
-      path: 'https://localhost:7162',
+      path: 'https://sicofaqa.cali.gov.co:4443/api',
       JWT_TOKEN: 'JWT_TOKEN',
       USER_INFO: 'USER_INFO',
       PERFILES_COMISARIAS: 'PERFILES_COMISARIAS',
