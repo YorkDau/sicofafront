6fae3d5a67e9005a8b29a3455d3a7efe946ca23f
York Dau <yordau10@gmail.com>
2025-04-04 03:04:46 -0500

    MERGE: Feature/mejoras into Develop

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
   name: 'development',
-  //path: 'https://localhost:7162/api',
-  path: 'http://66.94.108.233:443/api',
+  path: 'https://localhost:7162/api',
+  //path: 'http://66.94.108.233:443/api',
   JWT_TOKEN: 'JWT_TOKEN',
   USER_INFO: 'USER_INFO',
   PERFILES_COMISARIAS: 'PERFILES_COMISARIAS',
